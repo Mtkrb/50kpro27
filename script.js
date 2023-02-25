86168cd693db8f88edc7eabfb24d0d26628d0d7a
Bektemir Abilov <younghearth@gmail.com>
2023-02-25 19:50:40 +0600

    project50k

---
+++ script.js
@@ -0,0 +1,83 @@
+// let object = { name: "beka", lastname: "abilov", age: 24 }
+
+// for (let key in object) {
+//   console.log("ключ: " + key, "значение: " + object[key])
+// }
+
+// let arrayName = ["beka", "24", "25"]
+
+// arrayName.forEach(function (entry) {
+//   console.log(entry)
+// })
+
+// 1
+// let username = prompt("Ваше имя?", "")
+// let userLastname = prompt("Ваше Фамилия?", "")
+// alert(username + " " + userLastname)
+// 2
+// let number1 = prompt("Введите первое число", "")
+// let number2 = prompt("Введите второе число", "")
+// alert(Number(number1) + Number(number2))
+// 3
+// let number1 = prompt("Введите первое число", "")
+// let number2 = prompt("Введите второе число", "")
+// alert(number1 * number2)
+
+///////////////////////////////////////////
+
+// let number1 = prompt("Введите первое число", "")
+// let operator = prompt("Введите нужный оператор", "")
+// let number2 = prompt("Введите второе число", "")
+
+// if (operator == "/") {
+//   alert(Number(number1) / Number(number2))
+// } else if (operator == "*") {
+//   alert(Number(number1) * Number(number2))
+// } else if (operator == "-") {
+//   alert(Number(number1) - Number(number2))
+// } else if (operator == "+") {
+//   alert(Number(number1) + Number(number2))
+// } else if (operator == "%") {
+//   alert(Number(number1) % Number(number2))
+// } else if (operator == "**") {
+//   alert(Number(number1) ** Number(number2))
+// } else {
+//   alert("Проверьте правильность заполнения данных")
+// }
+
+// switch (operator) {
+//   case "/":
+//     alert(Number(number1) / Number(number2))
+//     break
+//   case "+":
+//     alert(Number(number1) + Number(number2))
+//     break
+//   case "-":
+//     alert(Number(number1) - Number(number2))
+//     break
+//   case "*":
+//     alert(Number(number1) * Number(number2))
+//     break
+//   case "**":
+//     alert(Number(number1) ** Number(number2))
+//     break
+//   case "%":
+//     alert(Number(number1) % Number(number2))
+//     break
+//   default:
+//     alert("Проверьте правильность заполнения данных")
+// }
+
+// let admin = "admin"
+// let pass = "1234"
+
+// let user = prompt("Введите ваше имя", "")
+// let userPass = prompt("Введите пароль", "")
+
+// if (user !== admin) {
+//   alert("Имя пользователя введено не верно")
+// } else if (pass !== userPass) {
+//   alert("Пароль введен не верно")
+// } else {
+//   alert("Успешный вход")
+// }

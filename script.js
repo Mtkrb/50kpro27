8bb39876c7acc114fc76ac911aa01c9f2225a8cd
Bektemir Abilov <younghearth@gmail.com>
2023-03-07 10:34:30 +0600

    getting product data

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict"
 
 let productName = document.getElementsByClassName("product-name")
-productName[0].innerHTML
 console.log(productName[0].innerHTML)
 let productImage = document.getElementsByClassName("product-image")
 console.log(productImage[0])
+let productPrice = document.getElementsByClassName("product-price")
+console.log(productPrice[0])
